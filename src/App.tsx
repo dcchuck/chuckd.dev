5408545a26c958df873bb05f51f3ed1cf4911d8a
Chuck Danielsson <chuck.danielsson@gmail.com>
2019-03-03 23:31:02 -0500

    Homepage v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import github from './github.svg';
 import './App.css';
+
+const Github = () =>
+  <a
+    href="https://github.com/dcchuck"
+  >
+    <img src={github} className="Icon" />
+  </a>
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Name">
+          <h1>Chuck</h1>
+          <h1>Danielsson</h1>
+        </div>
+        <div className="Details">
+          <div className="NavDesktop">
+            <Github />
+          </div>
+          <h5 className="Gig">developer</h5>
+          <div className="NavMobile">
+            <Github />
+          </div>
+        </div>
       </div>
     );
   }
