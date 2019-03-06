88be9e69e4c6e065021d1657081f673c7eb15f3c
Chuck Danielsson <chuck.danielsson@gmail.com>
2019-03-06 01:06:40 -0500

    Inital about page implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+/**
+ * icons
+ */
+import home from './home.svg';
 import github from './github.svg';
+import user from './user.svg';
+
+import { languages, tools } from './logos'
+
+/**
+ * stylesheet
+ */
 import './App.css';
+
+const Home = () =>
+  <Link to="/">
+    <img src={home} className="Icon" />
+  </Link>
 
 const Github = () =>
   <a
@@ -9,24 +27,77 @@ const Github = () =>
     <img src={github} className="Icon" />
   </a>
 
+const User = () =>
+  <Link to="/about">
+    <img src={user} className="Icon" />
+  </Link>
+
+const HomeScreen = () =>
+  <div className="App">
+    <div className="Name">
+      <h1>Chuck</h1>
+      <h1>Danielsson</h1>
+    </div>
+    <div className="Details">
+      <div className="NavDesktop">
+        <User />
+        <Github />
+      </div>
+      <h5 className="Gig">developer</h5>
+      <div className="NavMobile">
+        <Github />
+      </div>
+    </div>
+  </div>
+
+const SummarySectionTitle = (props: any) =>
+  <div className="SummarySectionTitle">{props.children}</div>
+
+const SummarySection = (props: any) =>
+  <div className="SummarySection">{props.children}</div>
+
+const Logo = (props: any) => <img className="Logo" src={props.src} />
+
+const ToolLogos = () => languages.map(( e, i ) => <Logo src={e} key={i} />)
+const LanguageLogos = () => tools.map(( e, i ) => <Logo src={e} key={i} />)
+
+const AboutNav = () =>
+  <div className="AboutHeader">
+    <div className="AboutTitle">ChuckD</div>
+    <div className="AboutNav"><Home /></div>
+  </div>
+
+const Summary = () =>
+  <div className="Summary">
+    <SummarySectionTitle>
+      Enjoys building things in languages like...
+    </SummarySectionTitle>
+    <SummarySection>
+      { LanguageLogos() }
+    </SummarySection>
+    <SummarySectionTitle>
+      Leveraging tools and frameworks like...
+    </SummarySectionTitle>
+    <SummarySection>
+      { ToolLogos() }
+    </SummarySection>
+  </div>
+
+const About = () =>
+  <div className="About">
+    <AboutNav />
+    <Summary />
+  </div>
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="Name">
-          <h1>Chuck</h1>
-          <h1>Danielsson</h1>
+      <Router>
+        <div>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/about/" exact component={About} />
         </div>
-        <div className="Details">
-          <div className="NavDesktop">
-            <Github />
-          </div>
-          <h5 className="Gig">developer</h5>
-          <div className="NavMobile">
-            <Github />
-          </div>
-        </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/logos/index.tsx
@@ -0,0 +1,20 @@
+/**
+ * languages
+ */
+
+import javascript from './javascript.svg';
+import ruby from './ruby.svg';
+
+const languages = [ javascript, ruby ];
+
+/**
+ * tools
+ */
+
+import postgresql from './postgresql.svg';
+import Rails from './Ruby_On_Rails_Logo.svg';
+import redis from './redis.svg';
+
+const tools = [ Rails, redis, postgresql ];
+
+export { languages, tools };
