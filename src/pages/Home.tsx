a49064c6a7f664122b663d6c73841ac264b35bb3
Chuck Danielsson <chuck.danielsson@gmail.com>
2025-10-25 17:02:03 -0400

    remove developer line

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,12 @@ const GithubLink = () => (
 
 export const Home = () => (
   <div className="bg-seafoamgreen h-dvh p-8 overflow-x-hidden">
-    <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-title text-white break-words hyphens-manual" lang="en">
+    <h1
+      className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-title text-white break-words hyphens-manual"
+      lang="en"
+    >
       Chuck Dan&shy;iels&shy;son
     </h1>
-    <h2 className="text-xl font-title text-right text-white">developer</h2>
     <div className="flex justify-end">
       <GithubLink />
     </div>
